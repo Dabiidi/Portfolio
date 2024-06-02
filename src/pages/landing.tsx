81672688ff68bc5321d4abf4b09ffd7ad040e930
Dabiidi <daveleonardengracia@gmail.com>
2024-06-02 23:13:54 +0800

    refactor: change text

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -12,10 +12,10 @@ const Home = () => {
     <HomeContainer>
       <Navbar />
       <Title>Welcome to My Portfolio</Title>
-      <Subtitle>Frontend Developer & Designer</Subtitle>
+      <Subtitle>Frontend Developer</Subtitle>
       <Content>
         <p>
-          Hello! I'm [Your Name], a passionate frontend developer with a knack
+          Hello! I'm Dave Leonard, a passionate frontend developer with a knack
           for creating beautiful and functional web applications. Check out my
           projects below and feel free to get in touch!
         </p>
